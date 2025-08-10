08307f0a6a17b7e4ee5d14fb8d8bd2ec9693f469
LIGECT <viminiko@gmail.com>
2025-08-10 22:17:42 +0300

    Make the main page through DOM and style it

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 import "./style.css";
-import Background from "./Photoroom.png";
+import Face from "./face";
 
-function component() {
-  const content = document.getElementById("content");
-
-  const heading = document.createElement("h1");
-  heading.textContent = "Welcome to  Lumino";
-  const mainBackground = new Image();
-  mainBackground.classList.add("main-background");
-  // mainBackground.src = Background;
-  content.appendChild(mainBackground);
-  content.appendChild(heading);
-  return content;
-}
-
-document.body.appendChild(component());
+document.body.appendChild(Face());
