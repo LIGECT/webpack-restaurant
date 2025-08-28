64bcf6c22c2ca2cef428ef7fd66b5868798d3ba9
LIGECT <viminiko@gmail.com>
2025-08-28 19:37:08 +0300

    Make a header block on the About Us page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import "./style.css";
 import Face from "./pages/first-page/face";
 import Menu from "./pages/second-page/menu";
-// import About from "./pages/third-page/about";
+import About from "./pages/third-page/about";
 
 const contentDiv = document.getElementById("content");
 
 const homeButton = document.getElementById("home-button");
 const menuButton = document.getElementById("menu-button");
-// const aboutButton = document.getElementById("about-button");
+const aboutButton = document.getElementById("about-button");
 
 function clearBody() {
   contentDiv.innerHTML = "";
@@ -43,8 +43,9 @@ function loadPage(pageComponent, pageClass, withTransition = true) {
 
 homeButton.addEventListener("click", () => loadPage(Face, "page-home"));
 menuButton.addEventListener("click", () => loadPage(Menu, "page-menu"));
-// homeButton.addEventListener("click", () => loadPage(About));
+aboutButton.addEventListener("click", () => loadPage(About, "page-about"));
 
 // contentDiv.appendChild(Face());
 // loadPage(Face, "page-home", false);
-loadPage(Menu, "page-menu", false);
+// loadPage(Menu, "page-menu", false);
+loadPage(About, "page-about", false);
---
+++ src/pages/third-page/about.js
@@ -0,0 +1,19 @@
+import "./about.css";
+
+export default function createAboutPage() {
+  const fragment = document.createDocumentFragment();
+
+  const heroSection = document.createElement("header");
+  heroSection.classList.add("about-header");
+
+  const heading = document.createElement("h1");
+  heading.textContent = "About Us";
+
+  const subtitle = document.createElement("h2");
+  subtitle.textContent = "History of Taste and Light";
+
+  heroSection.append(heading, subtitle);
+
+  fragment.append(heroSection);
+  return fragment;
+}
